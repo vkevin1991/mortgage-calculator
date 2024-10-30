f67cc0970ec9161d16e07d49755d6c8a6c927fe4
Kevin Vargas <kevinvargas@Kevins-Laptop.local>
2024-10-30 17:52:24 -0600

    Test change default values

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,25 +2,29 @@ import express from "express";
 import cors from "cors";
 import mortgageRoutes from "./routes/mortgage.routes";
 
-const PORT = 3000;
+const PORT = 8080;
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use("/api/mortgage", mortgageRoutes);
 
-if (["production"].includes(process.env.NODE_ENV || "")) {
-  app.use(express.static("client/dist"));
+// if (["production"].includes(process.env.NODE_ENV || "")) {
+//   app.use(express.static("client/dist"));
 
-  const path = require("path");
-  app.get("*", (_, res) => {
-    res.sendFile(path.resolve("client", "dist", "index.html"));
-  });
-} else {
-  app.get("*", (req, res) => {
-    res.send("Mortgage API!");
-  });
-}
+//   const path = require("path");
+//   app.get("*", (_, res) => {
+//     res.sendFile(path.resolve("client", "dist", "index.html"));
+//   });
+// } else {
+//   app.get("*", (req, res) => {
+//     res.send("Mortgage API!");
+//   });
+// }
+
+app.get("*", (req, res) => {
+  res.send("Mortgage API!");
+});
 
 app.listen(PORT, () => {
   console.log("Server is running on port 3000");
