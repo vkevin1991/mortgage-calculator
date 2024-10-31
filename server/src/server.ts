525e9a80e59a312428419fe6a14be18dc1abfe4e
Kevin Vargas <kevinvargas@Kevins-Laptop.local>
2024-10-30 18:01:09 -0600

    Undo some changes

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,23 +9,23 @@ app.use(express.json());
 app.use(cors());
 app.use("/api/mortgage", mortgageRoutes);
 
-// if (["production"].includes(process.env.NODE_ENV || "")) {
-//   app.use(express.static("client/dist"));
+if (["production"].includes(process.env.NODE_ENV || "")) {
+  app.use(express.static("client/dist"));
 
-//   const path = require("path");
-//   app.get("*", (_, res) => {
-//     res.sendFile(path.resolve("client", "dist", "index.html"));
-//   });
-// } else {
-//   app.get("*", (req, res) => {
-//     res.send("Mortgage API!");
-//   });
-// }
+  const path = require("path");
+  app.get("*", (_, res) => {
+    res.sendFile(path.resolve("client", "dist", "index.html"));
+  });
+} else {
+  app.get("*", (req, res) => {
+    res.send("Mortgage API!");
+  });
+}
 
 app.get("*", (req, res) => {
   res.send("Mortgage API!");
 });
 
 app.listen(PORT, () => {
-  console.log("Server is running on port 3000");
+  console.log(`Server is running on port ${PORT}`);
 });
